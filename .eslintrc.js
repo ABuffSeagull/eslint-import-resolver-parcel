d68d6c281ba49e22a3ac01101100cc4f595f28bd
ABuffSeagull <reecevanatta@gmail.com>
2019-05-05 00:21:20 -0400

    Lots of changes!
    -Changed to my new eslint-config
    -Get rid of old toml file
    -Switch to module syntax
    -Add babel for testing
    -Bump minimum version for pika-pack
    -Fix problem with null config
    -Switch path manipulation entirely to path lib
    -Fix problem with aliases not being found

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,4 @@
-const { parse } = require('@iarna/toml');
-const { readFileSync } = require('fs');
-
-const config = parse(readFileSync('.eslintrc.toml'));
-config.rules.indent.push({ SwitchCase: 1 });
-module.exports = config;
+module.exports = {
+  parserOptions: { sourceType: 'module' },
+  extends: ['@abuffseagull/eslint-config-node', 'plugin:jest/recommended'],
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,218 +1,213 @@
-const importResolver = require('../src/index');
+/* eslint-disable no-sync */
 const path = require('path');
 const fs = require('fs');
+const importResolver = require('../src/index.js');
 
-describe('root level', () => {
-	test('resolves /test-folder/test-file.js successfully with specifying root dir', () => {
-		const source = '/test-folder/test-file';
-		const file = __filename;
-		const config = {
-			rootDir: 'test',
-		};
+describe('root level', function () {
+  test('resolves /test-folder/test-file.js successfully with specifying root dir', function () {
+    const source = '/test-folder/test-file';
+    const file = __filename;
+    const config = { rootDir: 'test' };
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-folder/test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-folder/test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file, config);
+    const actual = importResolver.resolve(source, file, config);
 
-		expect(actual).toEqual(expected);
-	});
-	test('resolves /test/test-file.js successfully', () => {
-		const source = '/test/test-file';
-		const file = __filename;
+    expect(actual).toEqual(expected);
+  });
+  test('resolves /test/test-file.js successfully', function () {
+    const source = '/test/test-file';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
+    expect(actual).toEqual(expected);
+  });
 });
 
-describe('package level', () => {
-	test('resolves ~/test/test-file.js with inner node_modules', () => {
-		const source = '~/test-file';
-		const file = __filename;
+describe('package level', function () {
+  test('resolves ~/test/test-file.js with inner node_modules', function () {
+    const source = '~/test-file';
+    const file = __filename;
 
-		const node_dir = __dirname + '/node_modules';
-		fs.mkdirSync(node_dir);
+    const nodeDir = path.resolve(__dirname, 'node_modules');
+    fs.mkdirSync(nodeDir);
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		fs.rmdirSync(node_dir);
-		expect(actual).toEqual(expected);
-	});
+    fs.rmdirSync(nodeDir);
+    expect(actual).toEqual(expected);
+  });
 
-	test('resolves ~/index.js with outer node_modules', () => {
-		const source = '~/package-test-file';
-		const file = __filename;
+  test('resolves ~/index.js with outer node_modules', function () {
+    const source = '~/package-test-file';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, '..', 'package-test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, '..', 'package-test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
+    expect(actual).toEqual(expected);
+  });
 
-	test('resolves ~/index.js with outer node_modules inside test-folder', () => {
-		const source = '~/package-test-file';
-		const file = path.resolve(__dirname, 'test-folder', 'index.js');
+  test('resolves ~/index.js with outer node_modules inside test-folder', function () {
+    const source = '~/package-test-file';
+    const file = path.resolve(__dirname, 'test-folder', 'index.js');
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, '..', 'package-test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, '..', 'package-test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
+    expect(actual).toEqual(expected);
+  });
 });
 
-describe('resolve', () => {
-	test('resolves ./test-file.js successfully', () => {
-		const source = './test-file';
-		const file = __filename;
+describe('resolve', function () {
+  test('resolves ./test-file.js successfully', function () {
+    const source = './test-file';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
+    expect(actual).toEqual(expected);
+  });
 
-	test('resolves index file inside directory successfully', () => {
-		const source = './test-folder';
-		const file = __filename;
+  test('resolves index file inside directory successfully', function () {
+    const source = './test-folder';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-folder/index.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-folder/index.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
-		expect(actual).toEqual(expected);
-	});
-	test('resolves core library successfully', () => {
-		const source = 'fs';
-		const file = __filename;
+    const actual = importResolver.resolve(source, file);
+    expect(actual).toEqual(expected);
+  });
+  test('resolves core library successfully', function () {
+    const source = 'fs';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: null,
-		};
+    const expected = {
+      found: true,
+      path: null,
+    };
 
-		const actual = importResolver.resolve(source, file);
-		expect(actual).toEqual(expected);
-	});
+    const actual = importResolver.resolve(source, file);
+    expect(actual).toEqual(expected);
+  });
 });
 
-describe('extensions', () => {
-	test('resolves another-test-file.ts with extensions successfully', () => {
-		const source = './another-test-file';
-		const file = __filename;
-		const config = {
-			extensions: ['.ts'],
-		};
+describe('extensions', function () {
+  test('resolves another-test-file.ts with extensions successfully', function () {
+    const source = './another-test-file';
+    const file = __filename;
+    const config = { extensions: ['.ts'] };
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'another-test-file.ts'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'another-test-file.ts'),
+    };
 
-		const actual = importResolver.resolve(source, file, config);
+    const actual = importResolver.resolve(source, file, config);
 
-		expect(actual).toEqual(expected);
-	});
-	test('does not resolve another-test-file.ts without extensions', () => {
-		const source = './another-test-file';
-		const file = __filename;
+    expect(actual).toEqual(expected);
+  });
+  test('does not resolve another-test-file.ts without extensions', function () {
+    const source = './another-test-file';
+    const file = __filename;
 
-		const expected = {
-			found: false,
-		};
+    const expected = { found: false };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
-	test('resolves jsx-test-file.jsx successfully', () => {
-		const source = './jsx-test-file.jsx';
-		const file = __filename;
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'jsx-test-file.jsx'),
-		};
+    expect(actual).toEqual(expected);
+  });
+  test('resolves jsx-test-file.jsx successfully', function () {
+    const source = './jsx-test-file.jsx';
+    const file = __filename;
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'jsx-test-file.jsx'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
-	test('resolves vue-test-file.vue successfully', () => {
-		const source = './vue-test-file.vue';
-		const file = __filename;
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'vue-test-file.vue'),
-		};
+    expect(actual).toEqual(expected);
+  });
+  test('resolves vue-test-file.vue successfully', function () {
+    const source = './vue-test-file.vue';
+    const file = __filename;
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'vue-test-file.vue'),
+    };
 
-		const actual = importResolver.resolve(source, file);
+    const actual = importResolver.resolve(source, file);
 
-		expect(actual).toEqual(expected);
-	});
+    expect(actual).toEqual(expected);
+  });
 });
 
-describe('aliases', () => {
-	test('resolves package alias successfully', () => {
-		const source = 'test-alias';
-		const file = __filename;
+describe('aliases', function () {
+  test('resolves package alias successfully', function () {
+    const source = 'test-alias';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, '../node_modules/jest/build/jest.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, '../node_modules/jest/build/jest.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
-		expect(actual).toEqual(expected);
-	});
-	test('resolves folder alias successfully', () => {
-		const source = 'folder-alias/test-file.js';
-		const file = __filename;
+    const actual = importResolver.resolve(source, file);
+    expect(actual).toEqual(expected);
+  });
+  test('resolves folder alias successfully', function () {
+    const source = 'folder-alias/test-file.js';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, 'test-folder/test-file.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, 'test-folder/test-file.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
-		expect(actual).toEqual(expected);
-	});
-	test('should resolve the correct alias', () => {
-		const source = 'foobar';
-		const file = __filename;
+    const actual = importResolver.resolve(source, file);
+    expect(actual).toEqual(expected);
+  });
+  test('should resolve the correct alias', function () {
+    const source = 'foobar';
+    const file = __filename;
 
-		const expected = {
-			found: true,
-			path: path.resolve(__dirname, '../node_modules/jest/build/jest.js'),
-		};
+    const expected = {
+      found: true,
+      path: path.resolve(__dirname, '../node_modules/jest/build/jest.js'),
+    };
 
-		const actual = importResolver.resolve(source, file);
-		expect(actual).toEqual(expected);
-	});
+    const actual = importResolver.resolve(source, file);
+    expect(actual).toEqual(expected);
+  });
 });
