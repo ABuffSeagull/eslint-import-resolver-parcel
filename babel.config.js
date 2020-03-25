018a0bd7659923a8604d418b84219b6f77465bed
ABuffSeagull <reecevanatta@gmail.com>
2020-03-24 21:03:10 -0400

    Change babel config to json

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = function config (api) {
-  const testConfig = { presets: [['@babel/preset-env', { targets: { node: 'current' } }]] };
-  return api.env('test') ? testConfig : {};
-};
